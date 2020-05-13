5396ebb0124ecdafe23b7fccb59d5d080da616b1
Eivind Madsen Pham <eivindmpham@gmail.com>
2020-05-13 18:58:22 +0200

    Added svelte-routing, removed hideMe function

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,38 @@
 <script>
+	import { Router, Link, Route } from 'svelte-routing'
 	import Header from './components/Header.svelte'
-	import SideNav from './components/SideNav.svelte'
-	import MenuItems from './components/MenuItems.svelte'
-</script>
+	import Education from './components/Education.svelte'
+	import Experience from './components/Experience.svelte'
+	import Interests from './components/Interests.svelte'
+	import Eivind from './components/Eivind.svelte'
+	// import SideNav from './components/SideNav.svelte'
+	// import MenuItems from './components/MenuItems.svelte'
 
+	export let url = "";
+</script>
+<Router url='{url}'>
 	<Header />
-	<MenuItems />
+	<nav class='side-nav'>
+		<div>
+			<Link to='/'><p class='menu-items'>Eivind</p></Link>
+			<Link to='education'><p class='menu-items'>Education</p></Link>
+			<Link to='experience'><p class='menu-items'>Experience</p></Link>
+			<Link to='interests'><p class='menu-items'>Interests</p></Link>
+		</div>
+	</nav>
+	<div class='link' >
+		<Route path='education' component='{Education}' />
+		<Route path='/experience' component='{Experience}' />
+		<Route path='/interests' component='{Interests}' />
+	</div>
+</Router>
+
+	<main>
+		<h1>Eivind</h1>
+	</main>
 
 <style>
+
 :global(*) {
 	box-sizing: border-box;
 }
@@ -17,4 +42,37 @@
 	padding: 0;
 	background-color: #1a1a1a;
 }
+
+.side-nav {
+	width: 300px;
+	height: 100vh;
+	margin-top: 100px;
+	background-color: #1a1a1a;
+	position: fixed;
+	display: grid;
+	place-items: center;
+}
+main {
+	height: 100vh;
+	width: 100vw;
+	display: grid;
+	place-items: center;
+}
+h1 {
+	font-size: 10rem;
+	color: white; 
+}
+
+.menu-items {
+    color: #fff;
+    font-family: 'Eczar', serif;
+    font-size: 1.5rem;
+    font-weight: 100;
+    letter-spacing: .1rem;
+    cursor: pointer;
+    margin-left: 2rem;
+		text-decoration: none;
+  }
+	
+
 </style>
---
+++ src/components/Interests.svelte
@@ -0,0 +1,16 @@
+<main>
+  <h1>Interests</h1>
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    place-items: center;
+  }
+
+  h1 {
+    font-size: 10rem;
+  }
+</style>
